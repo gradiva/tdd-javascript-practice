74bee280ab45d371d4a784149bca6b6b6b1decfd
Leticia <leticia.campos.cm@gmail.com>
2019-10-23 21:23:18 +0200

    Change toBe assertion to toMatchObject

--- src/array-analysis/analyze.test.js
+++ src/array-analysis/analyze.test.js
@@ -2,7 +2,7 @@ import { analyze } from './analyze';
 
 describe('Tests for analyze array', () => {
   it('should return the correct values for array 1', () => {
-    expect(analyze([1, 1, 2, 3, 5, 8])).toBe({
+    expect(analyze([1, 1, 2, 3, 5, 8])).toMatchObject({
       average: 3,
       min: 1,
       max: 8,
@@ -11,7 +11,7 @@ describe('Tests for analyze array', () => {
   });
 
   it('should return the correct values for array 2', () => {
-    expect(analyze([1, 5, 5, 25, 125, 3125])).toBe({
+    expect(analyze([1, 5, 5, 25, 125, 3125])).toMatchObject({
       average: 548,
       min: 1,
       max: 3125,
